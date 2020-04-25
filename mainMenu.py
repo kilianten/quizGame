fbfb1d2d833578038ba656afa990390704c11dfa
kilian <kiliantennyson@live.ie>
2020-04-25 15:33:52 +0100

    Added exit game button and fuctionality tto menu

--- mainMenu.py
+++ mainMenu.py
@@ -6,6 +6,7 @@ class mainMenu:
         self.game = game
         self.sprites = game.menu_sprites
         self.newGameTile = MainMenuTile(game, 11 * game.tilesizeWidth, game.tilesizeHeight * 6, NEW_GAME_TEXT)
+        self.exitGameTile = MainMenuTile(game, 11 * game.tilesizeWidth, game.tilesizeHeight * 18, EXIT_GAME_TEXT)
 
     def update(self):
         self.sprites.update()
@@ -13,3 +14,5 @@ class mainMenu:
             self.game.texts = self.game.game_texts
             self.game.current_sprites = self.game.game_sprites
             self.game.collidables = self.game.collidable_sprites
+        if self.exitGameTile.clicked:
+            self.game.quit()
